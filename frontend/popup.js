25249f68a8f6fb222222371f642963b1ce8361d6
Louis Jurdant <ljurdant@student.42.fr>
2024-11-10 18:02:29 +0100

    set up gemini api connection

--- frontend/popup.js
+++ frontend/popup.js
@@ -1,7 +1,18 @@
+import { GoogleGenerativeAI } from "./node_modules/@google/generative-ai/dist/index.mjs";
+const genAI = new GoogleGenerativeAI("AIzaSyBoaHgH2fhKTtuHloOHqHP8Qsbjfbvgysg");
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
 const button = document.getElementById("mybutton");
 button.addEventListener("click", async () => {
   const [tab] = await chrome.tabs.query({ active: true });
   const response = await chrome.tabs.sendMessage(tab.id, { greeting: "hello" });
-  alert(response.body);
-  console.log(response.body);
+  alert("analyzing...");
+
+  const prompt = `Parse the following html page and return the article within this page in the form of a text:${response.body}`;
+
+  const result = await model.generateContent(prompt);
+  alert(result.response.text());
+  const element = document.getElementById("response");
+  const textNode = document.createTextNode(result.response.text());
+  element.appendChild(textNode);
 });
--- frontend/scripts/content.js
+++ frontend/scripts/content.js
@@ -1,9 +1,4 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  console.log(
-    sender.tab
-      ? "from a content script:" + sender.tab.url
-      : "from the extension"
-  );
   const main = document.body;
   if (request.greeting === "hello") sendResponse({ body: main.innerText });
 });
