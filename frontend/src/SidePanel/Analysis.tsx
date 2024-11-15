30f1ecc21e57794d2a289dceaf7e8a9051d2e755
Louis Jurdant <ljurdant@student.42.fr>
2024-11-15 13:22:22 +0100

    blind frontend

--- frontend/src/SidePanel/Analysis.tsx
+++ frontend/src/SidePanel/Analysis.tsx
@@ -1,4 +1,4 @@
-import { Button, Stack, Text } from "@mantine/core";
+import { Badge, Button, Grid, Group, Stack, Text } from "@mantine/core";
 import "../../styles/analysis.css";
 import { DelayedTextDisplay } from "./DelayedTextDisplay";
 import { ApiArticlesProcessResponseDto } from "../../types/api_articles_process.response.dto";
@@ -13,11 +13,25 @@ export const Analysis = ({ content }: Props) => {
       url: "index.html#tab",
     });
   };
-  const title = `{Main thesis: ${content.thesis}`;
+  const title = `Main thesis: ${content.thesis}`;
+  const foundArguments = `Found ${content.arguments.length} arguments`;
 
   return (
     <Stack>
       <DelayedTextDisplay text={title} />
+      <DelayedTextDisplay text={foundArguments} />
+      {content.arguments.map((argument, index) => {
+        const numberOfManipulations = Object.values(
+          argument.manipulations
+        ).reduce((acc, curr) => acc + curr.length, 0);
+        return (
+          <Group key={index}>
+            <Text>{argument.statement}</Text>
+            {numberOfManipulations && <Badge>{numberOfManipulations}</Badge>}
+          </Group>
+        );
+      })}
+
       <Button variant="subtle" onClick={handleClick}>
         Dig deeper
       </Button>
